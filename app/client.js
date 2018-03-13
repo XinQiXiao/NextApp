e5c24e34552bdec8cf26632de75e38499b518bd8
qixin <qixin@haoqix.com>
2018-03-13 17:48:23 +0800

    add iOS 启动图和icon

---
+++ app/client.js
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+import React, { Component } from 'react'
+
+// components
+import MainRoot from './container/Main'
+
+// TODO apiClient
+// TODO store 
+// TODO global storage
+
+export default class extends Component{
+	constructor(props){
+		super(props)
+	}
+
+	render(){
+		return <MainRoot />
+	}
+}
